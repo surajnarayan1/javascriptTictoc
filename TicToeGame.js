35a28626b64fdb9bde420708e0eb77069b54a48d
surajnarayan1 <snarayan@tavisca.com>
2017-08-27 13:03:44 +0530

    added html and js

---
+++ TicToeGame.js
@@ -0,0 +1,65 @@
+ 
+        function startGame()
+        {
+            document.turn = "X";
+            setMessage(document.turn +"start the game")
+           
+        }
+        function setMessage(msg)
+        {
+            document.getElementById("message").innerText = msg;
+        }
+        function nextMove(square)
+        {
+            if (square.innerText == "")
+            {
+                square.innerText = document.turn;
+                swithTurn();
+            }
+            else
+            {
+                setMessage("Inavalid Move");
+            }
+        }
+        function swithTurn()
+        {
+            
+            if (document.turn == "X") {
+                document.turn = "0";
+            }
+            else
+            {
+                document.turn = "X";
+            }
+           
+        }
+        function checkForWinner(move)
+        {
+            var result = false;
+            if (checkRow(1, 2, 3, move) ||
+                checkRow(4, 5, 6, move) ||
+                checkRow(7, 8, 9, move) ||
+                checkRow(1, 4, 7, move) ||
+                checkRow(2, 5, 8, move) ||
+                checkRow(3, 6, 9, move) ||
+                checkRow(1, 5, 9, move) ||
+                checkRow(3, 5, 7, move))
+            {
+                result = true;
+            }
+            return result;
+        }
+        function checkRow(a, b, c, move)
+        {
+            var result = false;
+            if (getBox(a) == move && getBox(b) == move & getBox(c) == move)
+            {
+                result = true;
+            }
+             
+        }
+        function getBox(number)
+        {
+            return document.getElementById("s" + number).innerText;
+        }
+ 
